1a224e85a88309870e34667a3d41066c3559f8f1
BluesJiang <763400095@qq.com>
2015-11-16 19:58:49 +0800

    first commit

---
+++ Pull To Refresh/BJRefreshView.h
@@ -0,0 +1,13 @@
+//
+//  BJRefreshView.h
+//  Pull To Refresh
+//
+//  Created by BluesJiang on 10/11/15.
+//  Copyright © 2015年 BluesJiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BJRefreshView : UIView
+
+@end
---
+++ Pull To Refresh/BJRefreshView.m
@@ -0,0 +1,21 @@
+//
+//  BJRefreshView.m
+//  Pull To Refresh
+//
+//  Created by BluesJiang on 10/11/15.
+//  Copyright © 2015年 BluesJiang. All rights reserved.
+//
+
+#import "BJRefreshView.h"
+
+@implementation BJRefreshView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
